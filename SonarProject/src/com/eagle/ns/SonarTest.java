0739ca61d2b09f16620637cd8e1598a99190a43a
nileshpatwardhan <34732752+nileshpatwardhan@users.noreply.github.com>
2019-08-24 12:23:37 +0530

    FirstCommit

---
+++ SonarProject/src/com/eagle/ns/SonarTest.java
@@ -0,0 +1,18 @@
+package com.eagle.ns;
+
+public class SonarTest {
+	
+	SonarTest() {
+		System.out.println("This is constructor");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new SonarTest();
+	}
+	
+	private void foo() {
+		
+	}
+
+}
